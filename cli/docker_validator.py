4b1a79e9e03735c53beb4ffc05da063ea3f333b4
nkumar2 <nkumar2@ebi.ac.uk>
2023-02-22 23:28:01 +0000

    review comments incorporated and tests added

--- cli/docker_validator.py
+++ cli/docker_validator.py
@@ -1,12 +1,13 @@
 import argparse
 import csv
 import logging
+import os
 import subprocess
-from pathlib import Path
+import time
 
 logging.basicConfig(format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p', level='INFO')
 
-container_image = 'eva_pre_submission_validator'
+container_image = 'eva_sub_cli'
 container_validation_dir = '/opt/vcf_validation'
 container_validation_output_dir = '/opt/vcf_validation/vcf_validation_output'
 
@@ -15,8 +16,8 @@ def run_command_with_output(command_description, command, return_process_output=
                             log_error_stream_to_output=False):
     process_output = ""
 
-    logging.info("Starting process: " + command_description)
-    logging.info("Running command: " + command)
+    logging.info(f"Starting process: {command_description}")
+    logging.info(f"Running command: {command}")
 
     stdout = subprocess.PIPE
     # Some utilities output non-error messages to error stream. This is a workaround for that
@@ -34,24 +35,31 @@ def run_command_with_output(command_description, command, return_process_output=
                 line = str(line).rstrip()
                 logging.error(line)
     if process.returncode != 0:
-        logging.error(command_description + " failed! Refer to the error messages for details.")
+        logging.error(f"{command_description} - failed! Refer to the error messages for details.")
         raise subprocess.CalledProcessError(process.returncode, process.args)
     else:
-        logging.info(command_description + " - completed successfully")
+        logging.info(f"{command_description} - completed successfully")
     if return_process_output:
         return process_output
 
 
 def check_if_file_missing(mapping_file):
     files_missing = False
+    missing_files_list = []
     with open(mapping_file) as open_file:
         reader = csv.DictReader(open_file, delimiter=',')
         for row in reader:
-            if not Path(row['vcf']).is_file():
+            if not os.path.exists(row['vcf']):
                 files_missing = True
-                logging.error('%s does not exist', row['vcf'])
+                missing_files_list.append(row['vcf'])
+            if not os.path.exists(row['fasta']):
+                files_missing = True
+                missing_files_list.append(row['fasta'])
+            if not os.path.exists(row['report']):
+                files_missing = True
+                missing_files_list.append(row['report'])
 
-    return files_missing
+    return files_missing, missing_files_list
 
 
 def verify_docker_is_installed(docker):
@@ -96,10 +104,10 @@ def try_restarting_container(docker, container_name):
 def verify_image_available_locally(docker, container_image):
     container_images_cmd_ouptut = run_command_with_output("Check if validator image is present", f"{docker} images")
     if container_images_cmd_ouptut is not None and container_image in container_images_cmd_ouptut:
-        logging.info(f"Container ({container_name}) image is available locally")
+        logging.info(f"Container ({container_image}) image is available locally")
         return True
     else:
-        logging.info(f"Container ({container_name}) image is not available locally")
+        logging.info(f"Container ({container_image}) image is not available locally")
         return False
 
 
@@ -107,7 +115,9 @@ def run_container(docker, container_name):
     logging.info(f"Trying to run container {container_name}")
     try:
         run_command_with_output("Try running container",
-                                f"{docker} run -it -d --name {container_name} {container_image}")
+                                f"{docker} run -it --rm -d --name {container_name} {container_image}")
+        # stopping execution to give some time to container to get up and running
+        time.sleep(5)
         if not verify_container_is_running(docker, container_name):
             raise RuntimeError(f"Container ({container_name}) could not be started")
     except subprocess.CalledProcessError as ex:
@@ -138,9 +148,10 @@ def verify_docker_env(docker, container_name):
             else:
                 download_container_image(docker, container_name)
 
+
 def copy_files_to_container(docker, container_name, mapping_file):
     run_command_with_output("Create directory structure for copying vcf metadata file into container",
-                            f"{docker} exec {container_name} mkdir -p {container_validation_dir}/{mapping_file.parent}")
+                            f"{docker} exec {container_name} mkdir -p {container_validation_dir}/{os.path.dirname(mapping_file)}")
     run_command_with_output("Copy vcf metadata file to container",
                             f"{docker} cp {mapping_file} {container_name}:{container_validation_dir}/{mapping_file}")
 
@@ -149,37 +160,31 @@ def copy_files_to_container(docker, container_name, mapping_file):
         for row in reader:
             run_command_with_output("Create directory structure to copy vcf files into container",
                                     f"{docker} exec {container_name} "
-                                    f"mkdir -p {container_validation_dir}/{Path(row['vcf']).parent}")
+                                    f"mkdir -p {container_validation_dir}/{os.path.dirname(row['vcf'])}")
             run_command_with_output("Copy vcf file to container",
                                     f"{docker} cp {row['vcf']} {container_name}:{container_validation_dir}/{row['vcf']}")
             run_command_with_output("Create directory structure to copy fasta files into container",
                                     f"{docker} exec {container_name} "
-                                    f"mkdir -p {container_validation_dir}/{Path(row['fasta']).parent}")
+                                    f"mkdir -p {container_validation_dir}/{os.path.dirname(row['fasta'])}")
             run_command_with_output("Copy fasta file to container",
                                     f"{docker} cp {row['fasta']} {container_name}:{container_validation_dir}/{row['fasta']}")
+            run_command_with_output("Create directory structure to copy assembly report files into container",
+                                    f"{docker} exec {container_name} "
+                                    f"mkdir -p {container_validation_dir}/{os.path.dirname(row['report'])}")
+            run_command_with_output("Copy assembly report file to container",
+                                    f"{docker} cp {row['report']} {container_name}:{container_validation_dir}/{row['report']}")
 
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Run pre submission validation checks on vcf files', add_help=False)
-    parser.add_argument("--docker_path", help="full path to the docker installation, "
-                                              "not required if docker is available on path", required=False)
-    parser.add_argument("--container_name", help="name of the docker container", required=False)
-    parser.add_argument("--vcf_files_mapping",
-                        help="csv file with the mappings for vcf files, fasta and assembly report", required=True)
-    parser.add_argument("--output_dir", help="Directory where the validation output reports will be made available", required=True)
-    args = parser.parse_args()
-
-    docker = args.docker_path if args.docker_path else 'docker'
-    container_name = args.container_name if args.container_name else container_image
-
-    mapping_file = Path(args.vcf_files_mapping)
-    output_dir = Path(args.output_dir)
-
+def run_docker_validator(docker, container_name, mapping_file, output_dir):
     # verify mapping file exists
-    if not mapping_file.is_file():
+    if not os.path.exists(mapping_file):
         raise RuntimeError(f'Mapping file {mapping_file} not found')
-    elif check_if_file_missing(mapping_file):
-        raise RuntimeError('some files (vcf/fasta) mentioned in metadata file could not be found')
+
+    # verify all files mentioned in metadata files exist
+    files_missing, missing_files_list = check_if_file_missing(mapping_file)
+    if files_missing:
+        raise RuntimeError(f"some files (vcf/fasta) mentioned in metadata file could not be found. "
+                           f"Missing files list {missing_files_list}")
 
     # check if docker container is ready for running validation
     verify_docker_env(docker, container_name)
@@ -203,3 +208,20 @@ if __name__ == "__main__":
                                 f"{docker} cp {container_name}:{container_validation_output_dir} {output_dir}")
     except subprocess.CalledProcessError as ex:
         logging.error(ex)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Run pre-submission validation checks on VCF files', add_help=False)
+    parser.add_argument("--docker_path", help="Full path to the docker installation, "
+                                              "not required if docker is available on path", required=False)
+    parser.add_argument("--container_name", help="Name of the docker container", required=False)
+    parser.add_argument("--vcf_files_mapping",
+                        help="csv file with the mappings for vcf files, fasta and assembly report", required=True)
+    parser.add_argument("--output_dir", help="Directory where the validation output reports will be made available",
+                        required=True)
+    args = parser.parse_args()
+
+    docker_path = args.docker_path if args.docker_path else 'docker'
+    docker_container_name = args.container_name if args.container_name else container_image
+
+    run_docker_validator(docker_path, docker_container_name, args.vcf_files_mapping, args.output_dir)
---
+++ tests/docker_validator_test.py
@@ -0,0 +1,75 @@
+import csv
+import os
+import shutil
+from unittest import TestCase
+
+from cli.docker_validator import run_docker_validator
+
+
+class TestDockerValidator(TestCase):
+    resources_folder = os.path.join(os.path.dirname(__file__), 'resources')
+
+    vcf_files = os.path.join(resources_folder, 'vcf_files')
+    fasts_files = os.path.join(resources_folder, 'fasta_files')
+    assembly_reports = os.path.join(resources_folder, 'assembly_reports')
+
+    test_run_dir = os.path.join(resources_folder, 'docker_test_run')
+    mapping_file = os.path.join(test_run_dir, 'vcf_files_metadata.csv')
+    output_dir = os.path.join(test_run_dir, 'validation_output')
+
+    def setUp(self):
+        if not os.path.exists(self.test_run_dir):
+            os.makedirs(self.test_run_dir)
+
+        # create vcf metadata file
+        data = [
+            ['vcf', 'fasta', 'report'],
+            [os.path.join(self.vcf_files, 'input_passed.vcf'),
+             os.path.join(self.fasts_files, 'input_passed.fa'),
+             os.path.join(self.assembly_reports, 'input_passed.txt')]
+        ]
+        with open(self.mapping_file, 'w', encoding='UTF8') as f:
+            writer = csv.writer(f)
+            for row in data:
+                writer.writerow(row)
+
+    def tearDown(self):
+        if os.path.exists(self.test_run_dir):
+            shutil.rmtree(self.test_run_dir)
+
+    def test_docker_validator(self):
+        docker = 'docker'
+        container_name = 'test'
+
+        # run validation in docker
+        run_docker_validator(docker, container_name, self.mapping_file, self.output_dir)
+
+        # assert vcf checks
+        vcf_format_dir = os.path.join(self.output_dir, 'vcf_format')
+        self.assertTrue(os.path.exists(vcf_format_dir))
+
+        vcf_format_log_file = os.path.join(vcf_format_dir, 'input_passed.vcf.vcf_format.log')
+        self.assertTrue(os.path.exists(vcf_format_log_file))
+
+        with open(vcf_format_log_file) as vcf_format_log_file:
+            vcf_format_logs = vcf_format_log_file.readlines()
+            self.assertEquals('[info] According to the VCF specification, the input file is valid\n',
+                              vcf_format_logs[3])
+
+            text_report = vcf_format_logs[2].split(':')[1].strip()
+            with open(os.path.join(self.output_dir, text_report)) as text_report:
+                text_report_content = text_report.readlines()
+                self.assertEquals('According to the VCF specification, the input file is valid\n',
+                                  text_report_content[0])
+
+        # assert assembly report
+        assembly_check_dir = os.path.join(self.output_dir, 'assembly_check')
+        self.assertTrue(os.path.exists(assembly_check_dir))
+
+        assembly_check_log_file = os.path.join(assembly_check_dir, 'input_passed.vcf.assembly_check.log')
+        self.assertTrue(os.path.exists(assembly_check_log_file))
+
+        with open(assembly_check_log_file) as assembly_check_log_file:
+            assembly_check_logs = assembly_check_log_file.readlines()
+            self.assertEquals('[info] Number of matches: 247/247\n', assembly_check_logs[5])
+            self.assertEquals('[info] Percentage of matches: 100%\n', assembly_check_logs[6])
