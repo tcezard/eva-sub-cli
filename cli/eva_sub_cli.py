0d667d850984317ddc3405a569eb02439512ecd8
tcezard <tcezard@ebi.ac.uk>
2023-10-13 22:23:53 +0100

    Make eva_sub_cli.py executable

--- cli/eva_sub_cli.py
+++ cli/eva_sub_cli.py
@@ -1,3 +1,4 @@
+#!/bin/bash env
 import csv
 import os
 from argparse import ArgumentParser
--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
         'Programming Language :: Python :: 3'
     ],
     scripts=[
-        join(dirname(__file__), 'cli', 'samples_checker.py'),
-        join(dirname(__file__), 'cli', 'xlsx2json.py')
+        join(dirname(__file__), 'cli', s)
+        for s in ('xlsx2json.py', 'samples_checker.py', 'eva_sub_cli.py')
     ]
 )
